c9de40ad5d3f0c8aeb3ee909a5c752e9ddd7014a
Dini <ioluwande@gmail.com>
2022-09-05 15:54:12 +0100

    changes

--- apple/settings.py
+++ apple/settings.py
@@ -148,13 +148,6 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
-# Twilio SendGrid
-# EMAIL_HOST = 'smtp.sendgrid.net'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = 'apikey'
-# EMAIL_HOST_PASSWORD = config('SENDGRID_API_KEY')
-
 # Custom setting. To email
 RECIPIENT_ADDRESS = config('RECIPIENT_ADDRESS')
 
