2dcd99b23d0da3318c193f1ddad3a34c4a2bea3b
Dini <ioluwande@gmail.com>
2022-09-05 16:26:42 +0100

    changes

--- apple/settings.py
+++ apple/settings.py
@@ -124,7 +124,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 # STATICFILES_DIRS = (os.path.join(BASE_DIR, 'network/static/network'),)
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
