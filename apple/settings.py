267b34770b42bdf58065df4a8aeb44844970fcc4
Ifeoluwapo Kazeem Oluwande <ioluwande@gmail.com>
2022-09-05 20:44:44 +0100

    email host

--- apple/settings.py
+++ apple/settings.py
@@ -153,12 +153,12 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 SENDGRID_API_KEY = config('SENDGRID_API_KEY')
 
-EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'apikey'  # this is exactly the value 'apikey'
 EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
 EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-EMAIL_USE_SSL = True
+EMAIL_USE_TLS = True
+# EMAIL_USE_SSL = True
 
 # Custom setting. To email
 RECIPIENT_ADDRESS = config('RECIPIENT_ADDRESS')
